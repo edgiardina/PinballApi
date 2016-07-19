c35865747b62a72201fbe028f88a0e9f038120eb
Ed Giardina <ed@litmus.com>
2016-07-18 23:02:22 -0400

    First unit test

--- PinballApi.Tests/WPPRTestFixture.cs
+++ PinballApi.Tests/WPPRTestFixture.cs
@@ -1,14 +1,28 @@
 ﻿using NUnit.Framework;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace PinballApi.Tests
 {
     [TestFixture]
     class WPPRTestFixture
     {
-        public void Wppr_GetPlayer_ShouldReturnCorrectPlayer()
+        private PinballRankingApi rankingApi;
+
+        [SetUp]
+        public void SetUp()
         {
+            var apiKey = ConfigurationSettings.AppSettings["WPPRKey"];
+            rankingApi = new PinballRankingApi(apiKey);
+        }
 
+        [Test]
+        public async Task Wppr_GetPlayer_ShouldReturnCorrectPlayer()
+        {
+            int playerId = 6;
+            var player = await rankingApi.GetPlayerRecord(playerId);
 
+            Assert.That(player.Player.FirstName == "Josh");
         }
 
     }
