c2299cbe788ce9125010b862688c6934787ff02c
Ed Giardina <ed@edgiardina.com>
2022-08-24 18:15:20 -0400

    fixing women's ranking issue

--- PinballApi/Models/WPPR/v2/Rankings/WomensRankingResult.cs
+++ PinballApi/Models/WPPR/v2/Rankings/WomensRankingResult.cs
@@ -9,5 +9,10 @@ namespace PinballApi.Models.WPPR.v2.Rankings
     {
         [JsonProperty("current_wppr_rank")]
         public override int CurrentWpprRank { get; set; }
+
+        //There's a bad naming convention in the IFPA API so we get around it here. 
+        //When you call the women's ranking endpoint for women you get current_wppr_rank and for open you get current_rank
+        [JsonProperty("current_rank")]
+        private int CurrentRank { set { CurrentWpprRank = value; } }
     }
 }
