21a5e06d7aad33a1a40d4e13c4a57266bf4c9f29
Ed Giardina <ed@edgiardina.com>
2025-08-16 16:49:34 -0400

    Ignore test

--- PinballApi.Tests/PinballRankingApiTestFixture.cs
+++ PinballApi.Tests/PinballRankingApiTestFixture.cs
@@ -64,7 +64,7 @@ namespace PinballApi.Tests
             }
         }
 
-        [Test]
+        [Test, Ignore("Leagues not supported yet (returns 404)")]
         public async Task PinballRankingApi_Tournament_GetLeagues_ReturnsActiveLeagues()
         {
             var result = await rankingApi.GetLeagues(PinballApi.Models.WPPR.Universal.Tournaments.LeagueTimePeriod.Active);
